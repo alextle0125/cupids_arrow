a5ebddafee58c0ff2b80ca12da7c737aa49f18a3
Alexander Le <alextle0125>
2016-09-16 19:44:41 -0400

    init seed file and add location references for Items

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Item.find_or_create_by(name: "Macaroons", price: 8.8, likes_count: 1 + Random.rand(999))
+Item.find_or_create_by(name: "Pastries", price: 5.5, likes_count: 1 + Random.rand(999))
+Item.find_or_create_by(name: "Ice Cream", price: 3.5, likes_count: 1 + Random.rand(999))
